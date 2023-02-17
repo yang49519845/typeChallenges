7f8cabbd64a3b353f8c6efaa9d630916f9a2a545
FeiMiao <1342039458@qq.com>
2023-02-17 10:31:09 +0800

    Update 00533-easy-concat.ts
    Simple concat of two arrays

--- 00533-easy-concat.ts
+++ 00533-easy-concat.ts
@@ -13,9 +13,12 @@ type cases = [
 // type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U] 
 
 // ============= Your Code Here =============
-type Concat<T extends unknown[], U extends unknown[]> = 
-  T extends [...infer I]
-    ? U extends [...infer J]
-      ? [...I, ...J]
-      : [...I]
-    : []
+// type Concat<T extends unknown[], U extends unknown[]> = 
+//   T extends [...infer I]
+//     ? U extends [...infer J]
+//       ? [...I, ...J]
+//       : [...I]
+//     : []
+
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+
