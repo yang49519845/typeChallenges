7acd55ac9d4fad039bfcae5b08c1dd99e3ffe4ec
feimiao <1342039458@qq.com>
2023-02-21 16:17:23 +0800

    replace

---
+++ 00116-medium-replace.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type Result = Replace<'foobarbar', '', 'foo'>
+type Replace<S extends string, From extends string, To extends string> = S extends `${infer Rest}${From}` 
+? `${Rest}` extends S
+  ? S
+  : `${To}${Rest}`
+: S
