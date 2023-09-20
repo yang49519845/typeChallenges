ba82302980fa8213e543aab5be7e1250b5faddf4
feimiao <1342039458@qq.com>
2023-09-20 11:53:12 +0800

    github share

--- 03326-medium-bem-style-string.ts
+++ 03326-medium-bem-style-string.ts
@@ -3,29 +3,16 @@ import type { Equal, Expect } from './test-utils'
 
 type cases = [
   Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
-  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success' >>,
-  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large' >>,
+  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success'>>,
+  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large'>>,
 ]
 
 
 // ============= Your Code Here =============
 
-type GenerateLine<B extends string, E extends string[], Line extends string = '__'> = 
-  E extends [infer Head, ...infer Other]
-    ? Other extends string[]
-      ? Head extends string
-        ? `${B}${Line}${Head}` | GenerateLine<B, Other, Line>
-        : never
-      : never
-    : never
-
-
 type BEM<B extends string, E extends string[], M extends string[]> = 
-  M['length'] extends 0
-    ? E['length'] extends 0
-      ? B
-      : GenerateLine<B, E>
-    : E['length'] extends 0
-      ? GenerateLine<B, M, '--'>
-      : GenerateLine<GenerateLine<B, E>, M, '--'>
-
+  M extends [infer Head extends string, ...infer Other extends string[]]
+    ? `${BEM<B, E, []>}--${Head}` | BEM<B, E, Other extends [] ? never : Other>
+    : E extends [infer Head extends string, ...infer Other extends string[]]
+      ? `${BEM<B,[], M>}__${Head}` | BEM<B, Other extends [] ? never : Other, M>
+      : `${B}`
