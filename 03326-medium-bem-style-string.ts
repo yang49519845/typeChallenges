e951a8673102400ad249ef9529a84792b6db5a4d
feimiao <1342039458@qq.com>
2023-09-20 11:35:24 +0800

    remove repeat

--- 03326-medium-bem-style-string.ts
+++ 03326-medium-bem-style-string.ts
@@ -11,10 +11,10 @@ type cases = [
 // ============= Your Code Here =============
 
 type GenerateLine<B extends string, E extends string[], Line extends string = '__'> = 
-  E extends [infer EH, ...infer ET]
-    ? ET extends string[]
-      ? EH extends string
-        ? `${B}${Line}${EH}` | GenerateLine<B, ET, Line>
+  E extends [infer Head, ...infer Other]
+    ? Other extends string[]
+      ? Head extends string
+        ? `${B}${Line}${Head}` | GenerateLine<B, Other, Line>
         : never
       : never
     : never
@@ -24,7 +24,7 @@ type BEM<B extends string, E extends string[], M extends string[]> =
   M['length'] extends 0
     ? E['length'] extends 0
       ? B
-      : GenerateLine<B, E, '__'>
+      : GenerateLine<B, E>
     : E['length'] extends 0
       ? GenerateLine<B, M, '--'>
       : GenerateLine<GenerateLine<B, E>, M, '--'>
